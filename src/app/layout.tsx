d450a374dd2591b4ff2793adb3ba435fbb96f27f
Vasanth Srivatsa <vsnthdev@gmail.com>
2026-01-22 03:27:09 +0530

    ✨ change font to use Urbanist

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
+import { Urbanist } from 'next/font/google'
 
-const geistSans = Geist({
-    variable: '--font-geist-sans',
-    subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-    variable: '--font-geist-mono',
+const urbanist = Urbanist({
+    variable: '--font-urbanist',
     subsets: ['latin'],
 })
 
@@ -25,7 +20,7 @@ export default function RootLayout({
     return (
         <html lang='en'>
             <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+                className={`${urbanist.variable} antialiased`}
             >
                 {children}
             </body>
